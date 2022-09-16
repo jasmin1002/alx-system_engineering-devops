648cc6ca136dc681a97ff18113cff2405cc56635
Jasmine <jasmineolofin@gmail.com>
2022-09-16 10:35:07 -0700

    Added manifest file to manage school file: automation

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a managed file school
+file { 'school':
+ensure  => file,
+path    => '/tmp/school',
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0744',
+content => 'I love Puppet'
+}
