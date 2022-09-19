42c77b5fee8f9dc3eb474dd4d84aa900fa5c5f73
Jasmine <jasmineolofin@gmail.com>
2022-09-19 10:39:09 -0700

    Modified file path

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,14 @@
 # Client configuration file with puppet
-#
-$path_to_file = '/etc/ssh/ssh_config'
+#$path_to_file = '/etc/ssh/ssh_config'
 
 file_line {'Turn off passwd auth':
   ensure  => 'present',
-  path    => '${path_to_file}',
+  path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no'
 }
 
 file_line {'Declare identity file':
   ensure  => 'present',
-  path    => '${path_to_file}',
+  path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school'
 }
