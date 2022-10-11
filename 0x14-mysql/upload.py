04e48ffff6d3acc95419225c62cf302daf010e8f
Jasmine <jasmineolofin@gmail.com>
2022-10-11 11:14:15 -0700

    feat: Modify automation file to push to web-01

--- 0x14-mysql/upload.py
+++ 0x14-mysql/upload.py
@@ -4,4 +4,4 @@ from fabric.api import *
 env.hosts = ['web-01_ip_address', 'web-02_ip_address']
 
 def upload():
-    put("./setup_mysql_holberton_user.sql", "~/", use_sudo=True, mode='0744')
+    put("./create_table.sql", "~/", use_sudo=True, mode='0744')
