466f7e604519b6fd593f9de0b8d8282691ea0c7d
Jasmine <jasmineolofin@gmail.com>
2022-10-11 10:21:40 -0700

    feat: Add setup file for MySQL database

---
+++ 0x14-mysql/setup_mysql_holberton_user.sql
@@ -0,0 +1,7 @@
+-- Create a database user with following credentials:
+-- username: holberton_user
+-- password: projectcorrection280hbtn
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+
+-- Grant permission for holberton_user
+GRANT REPLICATION CLIENT ON *.* 'holberton_user'@'localhost';
