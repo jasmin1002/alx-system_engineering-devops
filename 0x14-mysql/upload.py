f4675d0dfe2c9674cc85e6b8b3ecd8599c530e8e
Jasmine <jasmineolofin@gmail.com>
2022-10-11 19:32:10 -0700

    feat: Modify automation file to pull MySQL schema

--- 0x14-mysql/upload.py
+++ 0x14-mysql/upload.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from fabric.api import *
 
-env.hosts = ['web-01_ip_address', 'web-02_ip_address']
+env.hosts = ['web-01_ip_address']
 
 def upload():
-    put("./create_table.sql", "~/", use_sudo=True, mode='0744')
+    put("./tyrell_corp.sql", "~/", use_sudo=True)
