8bb24e9dd69a4011789b1b818a2cb9cf5e117243
Jasmine <jasmineolofin@gmail.com>
2022-10-17 18:24:53 -0700

    feat: Add script that fetches data by API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''
+Documentation goes here
+'''
+
+import requests
+from sys import argv
+
+try:
+    id = argv[1]
+except IndexError:
+    print('Require employee\'s ID but none')
+
+domain = 'https://jsonplaceholder.typicode.com'
+todos = 'todos'
+users = 'users'
+user_id = {'userId': id}
+url_s = '{}/{}'.format(domain, users)
+res = requests.get(url_s)
+
+if res.status_code == 200:
+    users = res.json()
+    name = ''
+
+    for user in users:
+        if str(user.get('id')) == id:
+            name = user['name']
+            break
+
+    if name:
+        url_t = '{}/{}'.format(domain, todos)
+        res = requests.get(url_t, params=user_id)
+
+        if res.status_code == 200:
+            todos = res.json()
+
+            done = [data for data in todos if data['completed']]
+
+            msg = 'Employee {} is done with tasks({}/{})'\
+                .format(name, len(done), len(todos))
+            print(msg)
+
+            for completed in done:
+                msg = '\t {}'.format(completed['title'])
+                print(msg)
+        else:
+            print(res.status_code)
+else:
+    print(res.status_code)
