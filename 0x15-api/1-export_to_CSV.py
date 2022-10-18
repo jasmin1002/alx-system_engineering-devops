612d837ae4bf4c9120e8337dab62c965ab7e32ba
Jasmine <jasmineolofin@gmail.com>
2022-10-17 19:41:42 -0700

    feat: Add script to dump data csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+'''
+Documentation goes here
+'''
+
+import csv
+import requests
+from sys import argv
+
+try:
+    id = argv[1]
+except IndexError:
+    print('Require employee\'s ID but none')
+
+domain = 'https://jsonplaceholder.typicode.com'
+todos = 'todos'
+users = 'users'
+user_id = {'userId': id}
+url_s = '{}/{}'.format(domain, users)
+res = requests.get(url_s)
+
+if res.status_code == 200:
+    users = res.json()
+    name = ''
+
+    for user in users:
+        if str(user.get('id')) == id:
+            name = user['name']
+            break
+
+    if name:
+        url_t = '{}/{}'.format(domain, todos)
+        res = requests.get(url_t, params=user_id)
+
+        if res.status_code == 200:
+            todos = res.json()
+
+            nl = [
+                [
+                    data['userId'],
+                    name,
+                    data['completed'],
+                    data['title']
+                ] for data in todos
+            ]
+
+            with open('USER_ID.csv', 'w', encoding='utf-8') as w_file:
+                write = csv.writer(w_file, quoting=csv.QUOTE_ALL)
+                write.writerows(nl)
+
+        else:
+            print(res.status_code)
+else:
+    print(res.status_code)
